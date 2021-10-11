2ea61e3a303af78a14529cbba663572a41af116a
msaouter <msaouter@gmail.com>
2021-10-11 17:24:14 +0200

    [update]Bot is able to send message and answer commands

--- Bot_discord/Bot_discord/Bot_discord.py
+++ Bot_discord/Bot_discord/Bot_discord.py
@@ -9,5 +9,21 @@ bot = commands.Bot(command_prefix = "!", description = "Genshin bot")
 async def on_ready():
     print("Ready")
 
+@bot.command()
+async def coucou(ctx):
+    print("coucou")
+    await ctx.send("Coucou !") #await sert à attendre que le bot se connecte à discord (coroutine)
+
+@bot.command()
+async def serverInfo(ctx):
+    server = ctx.guild # guild = serveur
+    numberOfTxtChannels = len(server.text_channels)
+    numberOfVoiceChannels = len(server.voice_channels)
+    serverDescription = server.description
+    numberOfPerson = server.member_count
+    serverName = server.name
+    message = f"Le serveur **{serverName}** contient *{numberOfPerson}* personnes. \nLa description du serveur : {serverDescription}. \nCe serveur possède *{numberOfTxtChannels}* salons écrits et *{numberOfVoiceChannels}* salons vocaux."
+    await ctx.send(message)
+
 #run the bot
 bot.run("ODk3MTI0Njk1MTg5NjQ3NDEw.YWRGew.LZT2eCCrMzMRA4X5p9LKa1nKCSA")
